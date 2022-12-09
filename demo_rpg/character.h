8e8ca8163c40922ffd3e4095d2370ed8969925c0
sultanovN <104188736+sultanovN@users.noreply.github.com>
2022-12-09 21:02:24 +0600

    Добавьте файлы проекта.

---
+++ demo_rpg/character.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "hp.h"
+
+class Character
+{
+private:
+	Hp m_character_hp{5};
+	hpType m_damage;
+
+public:
+
+};
---
+++ demo_rpg/hp.h
@@ -0,0 +1,54 @@
+#pragma once
+#include "hitPointsTypes.h"
+
+class Hp
+{
+private:
+	hpType m_current_hp;
+	hpType m_max_hp;
+
+public:
+	Hp(hpType max_hp = 1)
+		: m_max_hp(max_hp), m_current_hp(max_hp)
+	{
+	}
+
+	//returns true if succesfully set
+	bool setMaxHp(hpType new_max_hp)
+	{
+		if (new_max_hp < 1)
+			return false;
+
+		m_max_hp = new_max_hp;
+
+		if (m_current_hp > m_max_hp)
+			m_current_hp = m_max_hp;
+
+		return true;
+	}
+
+	hpType getMaxHp() { return m_max_hp; }
+
+	hpType getCurrentHp() { return m_current_hp; }
+
+	void takeDamage(hpType damage)
+	{
+		if (damage > m_current_hp)
+		{
+			m_current_hp = 0;
+			return;
+		}
+
+	}
+
+	void heal(hpType amount)
+	{
+		if (amount + m_current_hp > m_max_hp)
+		{
+			m_current_hp = m_max_hp;
+			return;
+		}
+
+		m_current_hp += amount;
+	}
+};
